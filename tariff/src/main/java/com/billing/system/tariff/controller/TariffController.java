7f75f391a51115395d90acc7f67bbd721205e532
Spec163 <45912087+Spec163@users.noreply.github.com>
2020-08-17 19:48:16 +0400

    minor changes

--- tariff/src/main/java/com/billing/system/tariff/controller/TariffController.java
+++ tariff/src/main/java/com/billing/system/tariff/controller/TariffController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("admin/tariff")
+@RequestMapping("profile/tariff")
 @CrossOrigin
 public class TariffController {
 
